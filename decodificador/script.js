fec535c802ed914121041271bfc927b77a65cc97
victorxdking <victorxdking@gmail.com>
2024-02-20 20:52:31 -0300

    att

---
+++ decodificador/script.js
@@ -0,0 +1,14 @@
+
+let matriz_code = [
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat']
+];
+
+function btnCriptografar() {
+
+}
+
+function criptografar
